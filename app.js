6dc58d9720b366c74e61ff94a246b449427a9742
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-20 06:25:17 -0800

    Set up the upload and wait for the document type

--- app.js
+++ app.js
@@ -2,18 +2,21 @@
 
 // Load configuration and initialize server
 var restify = require('restify');
+
 var cluestrProvider = require('cluestr-provider');
 var serverConfig = require('./lib/provider-exact-target');
 
 var server = cluestrProvider.createServer(serverConfig);
 
+
+// Display the form
 server.get('/login', function (req, res, next) {
 
   var body = '<html><body>';
   body += '<form action="auth" method="post">';
   body += 'Login: <input type="text" name="login"><br>';
   body += 'Password: <input type="password" name="password"><br>';
-  body += '<input type="hidden" name="code" value="' + req.params.code + '">';
+  body += '<input type="hidden" name="state" value="' + req.params.state + '">';
   body += '<input type="submit" value="Submit">';
   body += '</form>';
   body += '</body></html>';
@@ -24,16 +27,12 @@ server.get('/login', function (req, res, next) {
   });
   res.write(body);
   res.end();
-
-  console.log('Display');
-
 });
 
-server.post('/auth', function(req, res, next) {
-  console.log(req.params);
-  
-  res.params = req.params;
-  res.header('Location', '/init/callback');
+// Post server for  the authencication
+server.post('/auth', function(req, res, next) {  
+  var redirectUrl = '/init/callback?state=' + req.params.state + '&login=' + req.params.login + '&password=' + req.params.password;
+  res.header('Location', redirectUrl);
   res.send(302);
   return next(false);
 });
