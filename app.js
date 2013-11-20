eb899101bd5dad0c072a4e36396304c19d55813f
Pierre-Marie Dartus <dartus.pierremarie@gmail.com>
2013-11-20 02:09:56 -0800

    Redirect with the right form

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
 "use strict";
 
 // Load configuration and initialize server
+var restify = require('restify');
 var cluestrProvider = require('cluestr-provider');
-var serverConfig = require('./lib/provider-PROVIDER');
+var serverConfig = require('./lib/provider-exact-target');
 
 var server = cluestrProvider.createServer(serverConfig);
+
+server.get('/login', function (req, res, next) {
+
+  var body = '<html><body>';
+  body += '<form action="auth" method="post">';
+  body += 'Login: <input type="text" name="login"><br>';
+  body += 'Password: <input type="password" name="password"><br>';
+  body += '<input type="hidden" name="code" value="' + req.params.code + '">';
+  body += '<input type="submit" value="Submit">';
+  body += '</form>';
+  body += '</body></html>';
+
+  res.writeHead(200, {
+    'Content-Length': Buffer.byteLength(body),
+    'Content-Type': 'text/html'
+  });
+  res.write(body);
+  res.end();
+
+  console.log('Display');
+
+});
+
+server.post('/auth', function(req, res, next) {
+  console.log(req.params);
+  
+  res.params = req.params;
+  res.header('Location', '/init/callback');
+  res.send(302);
+  return next(false);
+});
 
 // Expose the server
 module.exports = server;
